85443f55acdc247869ea2419bfed6660f81d9bb1
morten.aslesen <mortendaehli@gmail.com>
2022-12-02 11:46:50 +0100

    feat: day 2 solution

--- aoc/day2/__init__.py
+++ aoc/day2/__init__.py
@@ -1,17 +1,45 @@
 from aoc import logger
 
 if __name__ == "__main__":
-    logger.info("Starting")
+    logger.info("Day 2")
+
+    """
+    We map the whole outcome space into a dictionary since we only have 9 outcomes:
+    shape_scores:
+        X: 1  # Rock
+        Y: 2,  # Paper
+        Z: 3,  # Scissors
+
+    outcome_scores:
+        win: 6
+        draw: 3
+        lose: 0
+    """
+
+    strategy = {
+        "A": {"X": 3 + 1, "Y": 6 + 2, "Z": 0 + 3},  # Opponent Rock
+        "B": {"X": 0 + 1, "Y": 3 + 2, "Z": 6 + 3},  # Opponent Paper
+        "C": {"X": 6 + 1, "Y": 0 + 2, "Z": 3 + 3},  # Opponent Scissors
+    }
+
     with open("input.txt", "r") as file:
-        data = file.read().strip()
+        data = file.read().strip().split("\n")
 
-    result_part_1 = max([sum([int(x) for x in elf.split("\n")]) for elf in data.split("\n\n")])
-    logger.info(
-        f"Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?: {result_part_1}"
-    )
+    games = [game.split() for game in data]
+    result_part_1 = sum([strategy[opponent_shape][my_shape] for opponent_shape, my_shape in games])
 
-    result_part_2 = sum(sorted([sum([int(x) for x in elf.split("\n")]) for elf in data.split("\n\n")])[-3:])
     logger.info(
-        f"Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?: "
-        f"{result_part_2}"
+        f"What would your total score be if everything goes exactly according to your strategy guide?: {result_part_1}"
+    )  # 17189
+
+    new_strategy = {
+        "A": {"X": 0 + 3, "Y": 3 + 1, "Z": 6 + 2},  # Opponent Rock
+        "B": {"X": 0 + 1, "Y": 3 + 2, "Z": 6 + 3},  # Opponent Paper
+        "C": {"X": 0 + 2, "Y": 3 + 3, "Z": 6 + 1},  # Opponent Scissors
+    }
+
+    result_part_2 = sum([new_strategy[opponent_shape][my_shape] for opponent_shape, my_shape in games])
+    logger.info(
+        "Following the Elf's instructions for the second column, what would your total score be if everything goes "
+        f"exactly according to your strategy guide?: {result_part_2}"
     )
